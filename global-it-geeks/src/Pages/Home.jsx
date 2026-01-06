ef788f34799cf74bbe71c0272f4e29ab2f1ebf6f
kartik-sinha <kartiksinha520@gmail.com>
2026-01-06 22:49:07 +0530

    Fifth commit: Added Footer.jsx and Footer.css file

--- global-it-geeks/src/Pages/Home.jsx
+++ global-it-geeks/src/Pages/Home.jsx
@@ -33,7 +33,7 @@ export default function Home() {
             {/*<Trainings />*/}
             <Testimonials />
             {/*<PaymentCTA />*/}
-            {/*<Footer />*/}
+            <Footer />
         </>
     );
 }
